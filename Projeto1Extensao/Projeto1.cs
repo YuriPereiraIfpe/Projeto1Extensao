1ccdfd11c7fbc5eee6fc6796da534f6d8bf7be60
ytvp <ytvp@ERERE-014602>
2021-09-15 10:59:08 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto1Extensao/Projeto1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto1Extensao
+{
+    class Projeto1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Yuri T. Pereira!");
+            Console.ReadLine();
+            //coloquei o readline para a mensagem parar e ficar na tela
+            //e nao encerrar a execução
+        }
+    }
+}
